b60c1c7308d97657c9b7036ce6e5a01bbac75c72
momotsow <kgomotsomkansi@gmail.com>
2021-03-01 18:52:24 +0200

    potfolio files

---
+++ assets/js/script.js
@@ -0,0 +1,78 @@
+$( document ).ready(function() {
+                
+                $("#about_scroll").fadeOut();   
+                // $("#work_scroll").fadeOut();
+                $("#contact_scroll").fadeOut();
+
+                $("#about").click(function(){
+                    $("#index").fadeOut();
+                    $("#about_scroll").fadeIn();
+                    $('#about_left').addClass('animated slideInLeft');
+                    $('#about_right').addClass('animated slideInRight');
+                    });
+                // $("#work").click(function(){
+                //     $("#index").fadeOut();
+                //     $("#work_scroll").fadeIn();
+                //     $('#work_left').addClass('animated slideInLeft');
+                //     $('#work_right').addClass('animated slideInRight');
+                //     });
+                $("#contact").click(function(){
+                    $("#index").fadeOut();
+                    $("#contact_scroll").fadeIn();
+                    $('#contact_left').addClass('animated slideInLeft');
+                    $('#contact_right').addClass('animated slideInRight');
+                    });
+                
+                $(".back").click(function(){
+                    $(".pages").fadeOut();
+                    $("#index").fadeIn();
+                    $('#index_left').addClass('animated slideInLeft');
+                    $('#index_right').addClass('animated slideInRight');
+                    });
+           
+
+                    let currentSlide = 0;
+const slides = document.querySelectorAll(".slide")
+const dots = document.querySelectorAll('.dot')
+
+const init = (n) => {
+  slides.forEach((slide, index) => {
+    slide.style.display = "none"
+    dots.forEach((dot, index) => {
+      dot.classList.remove("active")
+    })
+  })
+  slides[n].style.display = "block"
+  slides[n].style.opacity = "1"
+  dots[n].classList.add("active")
+}
+document.addEventListener("DOMContentLoaded", init(currentSlide))
+const next = () => {
+  currentSlide >= slides.length - 1 ? currentSlide = 0 : currentSlide++
+  init(currentSlide)
+}
+
+const prev = () => {
+  currentSlide <= 0 ? currentSlide = slides.length - 1 : currentSlide--
+  init(currentSlide)
+}
+
+document.querySelector(".next").addEventListener('click', next)
+
+document.querySelector(".prev").addEventListener('click', prev)
+
+
+setInterval(() => {
+  next()
+}, 5000);
+
+dots.forEach((dot, i) => {
+  dot.addEventListener("click", () => {
+    console.log(currentSlide)
+    init(i)
+    currentSlide = i
+  })
+})
+
+
+		});
